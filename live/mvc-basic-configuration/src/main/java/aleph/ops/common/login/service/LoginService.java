4a028e7ea4bf5cfaba707f8b99e29a74ec496128
Tabish Junaid <tabishjunaid@gmail.com>
2018-04-29 20:57:54 +0530

    removed springmvc-basic-configuration to a different project

--- live/mvc-basic-configuration/src/main/java/aleph/ops/common/login/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package aleph.ops.common.login.service;
-
-public class LoginService {
-	
-	public Boolean verifyUser(String username, String password) {
-		return username.equals("Tabish") && password.equals("password");
-	}
-
-}
--- live/mvc-basic-configuration/src/main/webapp/WEB-INF/views/welcome.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<title>Welcome</title>
-	</head>
-	<body>
-		<p>Welcome ${name}</font>
-	</body>
-</html>
